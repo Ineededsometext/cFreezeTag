9c451b415c8493681c94adfa1e2267a66efeabfa
Ineededsometext <38172273+Ineededsometext@users.noreply.github.com>
2018-11-17 07:26:50 +0800

    Updated readme.lua

--- cFreeze Tag/gamemodes/cfreezetag/gamemode/readme.lua
+++ cFreeze Tag/gamemodes/cfreezetag/gamemode/readme.lua
@@ -1,28 +1,19 @@
 -- The official cFreeze Tag modification addon hasn't been created yet because this is still
 -- in public beta. It will be released soon when a lot of bugs has been fixed!
 
--- IGNORE
----------------------------------------------------------------------------------------------
---                                                                                         --
---                                                                                         --
---                     ______                            _______                           --
---                    |  ____|                          |__   __|                          --
---                ___ | |__  _ __  ___   ___  ____ ___     | |  __ _   __ _                --
---               / __||  __|| '__|/ _ \ / _ \|_  // _ \    | | / _` | / _` |               --
---              | (__ | |   | |  |  __/|  __/ / /|  __/    | || (_| || (_| |               --
---               \___||_|   |_|   \___| \___|/___|\___|    |_| \__,_| \__, |               --
---                                                                     __/ |               --
---                                                                    |___/                --
---                                                                                         --
---                                                                                         --
----------------------------------------------------------------------------------------------
+--[[ IGNORE
 
----------------------------------------------------------------------------------------------
---                                                                                         --
---                                                                                         --
---  DO NOT MESS WITH ANY FILES, USE THE OFFICIAL CFREEZE TAG MODIFICATION ADDON TO DO SO.  --
---        IF YOU MESSED WITH THE FILES AND BROKE EVERYTHING, IT WON'T BE MY FAULT!         --
---              SO DON'T ASK ME TO FIX THE MISTAKES THAT YOU HAVE DONE!                    --
---                                                                                         --
---                                                                                         --
----------------------------------------------------------------------------------------------
+                                                                                         
+                                                                                         
+                       ______                            _______                           
+                      |  ____|                          |__   __|                          
+                  ___ | |__  _ __  ___   ___  ____ ___     | |  __ _   __ _                
+                 / __||  __|| '__|/ _ \ / _ \|_  // _ \    | | / _` | / _` |               
+                | (__ | |   | |  |  __/|  __/ / /|  __/    | || (_| || (_| |               
+                 \___||_|   |_|   \___| \___|/___|\___|    |_| \__,_| \__, |               
+                                                                       __/ |               
+                                                                      |___/                                                                                                        --
+                                                                                         
+    DO NOT MESS WITH ANY FILES, USE THE OFFICIAL CFREEZE TAG MODIFICATION ADDON TO DO SO.  
+          IF YOU MESSED WITH THE FILES AND BROKE EVERYTHING, IT WON'T BE MY FAULT!         
+                SO DON'T ASK ME TO FIX THE MISTAKES THAT YOU HAVE DONE!                      ]]
