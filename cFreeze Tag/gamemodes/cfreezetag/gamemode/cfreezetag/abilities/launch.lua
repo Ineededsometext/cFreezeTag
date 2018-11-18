3524637ccdbebfa6558aaa5a2e08ae6e3652315a
Ineededsometext <38172273+Ineededsometext@users.noreply.github.com>
2018-11-18 15:15:59 +0800

    Created launch ability.

---
+++ cFreeze Tag/gamemodes/cfreezetag/gamemode/cfreezetag/abilities/launch.lua
@@ -0,0 +1,10 @@
+Ability.Name = "LAUNCH"
+Ability.Icon = "cfreeze_tag/icons/ability_launch.png"
+Ability.Team = TEAM_BOTH
+
+Ability.Uses = 1
+Ability.OnCast = function( ply, tr )
+    if ( CLIENT ) then return end
+
+    ply:SetVelocity( ply:GetAimVector() * 1000 )
+end
